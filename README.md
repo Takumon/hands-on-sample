ec49e557f69796508689b9d7b5a98feee70ccf87
Takumon <inouetakumon@gmail.com>
2020-11-07 10:53:36 +0900

    PointViewPageに要素を追加

--- README.md
+++ README.md
@@ -10,15 +10,19 @@
 - LoginPage
   - iOSで表示するとノッチ部分に「TopPage」が隠れてしまうので大枠のタグをViewからSafeAreaViewに変更
   - navigationはpropsから受け取らずuseNavigationを使うように変更
-- Login画面
- - ![実装完了時の画面スクショ](./images/v0.1.0.png)
-
+- 確認観点
+  - Login画面
+    - 表示できる
+    - レイアウトが以下のようになっている
+      - ![実装完了時の画面スクショ](./images/v0.1.0.png)
 
 ## v0.1.0...v0.2.0
 
 - ログイン画面で「v0.1.0」って表示しているのが気になった、 「v0.2.0」の間違い？
-- Login画面
-  - ![実装完了時の画面スクショ](./images/v0.2.0.png)
+- 確認観点
+  -  Login画面
+    - レイアウトが以下のようになっている
+      - ![実装完了時の画面スクショ](./images/v0.2.0.png)
 
 ## v0.2.0...v0.3.0
 
@@ -28,7 +32,16 @@
 - PointViewPage
   - iOSで表示するとノッチ部分に「TopPage」が隠れてしまうので大枠のタグをViewからSafeAreaViewに変更
   - navigationはpropsから受け取らずuseNavigationを使うように変更
-- Login画面
-  - ![実装完了時の画面スクショ](./images/v0.3.0_login.png)
-- PointView画面
-  - ![実装完了時の画面スクショ](./images/v0.2.0_pointview.png)
+- 確認観点
+  - Login画面
+    - ログインボタンを押すとPointView画面に遷移する
+  - PointView画面
+    - レイアウトが以下のようになっている
+      - ![実装完了時の画面スクショ](./images/v0.3.0_pointview.png)
+    - ログアウトボタンを押すとLogin画面に遷移する
+
+## v0.3.0...v0.4.0
+- 確認観点
+  - PointView画面
+    - レイアウトが以下のようになっている
+      - ![実装完了時の画面スクショ](./images/v0.4.0_pointview.png)
--- src/components/pages/PointViewPage.tsx
+++ src/components/pages/PointViewPage.tsx
@@ -1,13 +1,46 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {SafeAreaView, Text, Button} from 'react-native';
+import {SafeAreaView, View, Text, Button, Image, StyleSheet} from 'react-native';
+
+const sampleAvatar =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAABHNCSVQICAgIfAhkiAAAAAFzUkdCAK7OHOkAAAAEZ0FNQQAAsY8L/GEFAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABT0lEQVRYR+2WvXLCMAyAlcSUcvwMMHRhaF+gUwfef2mHvkMZGGCAAehRcNwiBTMlIEVue+HO3xL/xfddLEtJfhBoEKl/NoYoxBGFOKIQR3AeWuYOVnletEfGwNBkRVuLWijHt963nyjj/MgJEnrpdcEkfqAm6iN73WxLMgSNveGcFpXQx/4AG/fte2XWODc7WN+rh0poZU8xc43FfwpZQdhJ1lShEhpk/E2SrKlCJfR03/aty0jWVKES6qQJPHc7vleG5miNhqDESElx+rWHhQ/yh5aBR/wyo4DkGP8YOVRfiMrG2jk8sup8RDWNbpmmfNQSoliZYZY+xwwHxdS4fVc8pYiELhVSKXUKLitEMlQsqT6FMMhSmPR7rBQb1HNrg2UI2oP24mCFdr8gc0ay1+1d++IXVXirOIZ427gsrspDf0njjiwKcUQhjih0HYAj6K2fFtVoFLEAAAAASUVORK5CYII=';
+
+const styles = StyleSheet.create({
+  avatar: {
+    width: 50,
+    height: 50,
+  },
+});
 
 const PointViewPage: React.FC = () => {
   const navigation = useNavigation();
 
   return (
     <SafeAreaView>
-      <Text>PointViewPage</Text>
+      <View>
+        <Image source={{uri: sampleAvatar}} style={styles.avatar} />
+        <Text>山田 太郎</Text>
+      </View>
+
+      <View>
+        <View>
+          <Text>市内ゴミBOX回収総量</Text>
+          <Text>
+            <Text>1000</Text>
+            <Text>kg</Text>
+          </Text>
+        </View>
+        <View>
+          <Text>あなたが節約したゴミ袋の総額</Text>
+          <Text>
+            <Text>1280</Text>
+            <Text>円</Text>
+          </Text>
+        </View>
+      </View>
+
+      <Button title="ポイント実績" onPress={() => console.log('PointHistoryPage clicked!')} />
+
       <Button title="ログアウト" onPress={() => navigation.navigate('LoginPage')} />
     </SafeAreaView>
   );
